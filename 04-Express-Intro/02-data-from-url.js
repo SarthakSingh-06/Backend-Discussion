412532ccb060fdef0d2e33fe3474aae28575d818
Sarthak Singh <singhsarthak07072006@gmail.com>
2026-09-19 15:53:50 +0530

    learn about middlewares

--- 04-Express-Intro/02-data-from-url.js
+++ 04-Express-Intro/02-data-from-url.js
@@ -3,8 +3,27 @@ import express from "express";
 const app = express();
 const PORT = 8000;
 
-app.use(express.json({ limit: "8kb" })); // allow parsing the incoming json respnse
-app.use(express.urlencoded({ limit: "8kb" })); // allow parsing the incoming json respnse
+app.use(express.json({ limit: "16kb" })); // allow parsing the incoming json response
+app.use(express.urlencoded({ limit: "8kb" })); // allow parsing the incoming form data
+app.use(express.static("public")); // serve static files
+
+
+// Add middlewares
+app.use((req, res, next) => {
+    console.log("Middleware - 1");
+    res.send("Cycle terminated by Middleware - 1");
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log("Middleware - 2");
+    next();
+});
+
+app.get("/", (req, res) => {
+    res.setHeader("Content-Type", "text/html");
+    res.send("<h1>Home Page</h1>");
+});
 
 // dyamic routes
 app.get("/meeting/:meetId", (req, res) => {
